24a6a6bee4e7cf7fb0ad1aacac604d5c897851ed
Vincent Sellier <vsellier@exoplatform.com>
2017-02-03 17:36:19 +0100

    wip kubernetes

---
+++ kubernetes/bin/createConfigMapFromProperties.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -eu
+
+function printUsage() {
+  echo "$0 <ConfigMap name> <property file>"
+}
+
+if [ $# -ne 2 ]
+then
+  printUsage
+  exit 1
+fi
+
+PARAMS=""
+
+for line in $(cat $2)
+do
+  PARAMS="${PARAMS} --from-literal=${line}"
+done
+
+kubectl create configmap $1 ${PARAMS}
