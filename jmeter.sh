e7ff34d70a7149c2e2aa7ba721a277b1441fb544
Vincent Sellier <vsellier@exoplatform.com>
2017-11-20 19:20:20 +0100

    jenkinsfile for jmeter

---
+++ jmeter.sh
@@ -0,0 +1,71 @@
+#!/usr/bin/env bash
+
+[ "${DEBUG}" ] && set -x
+
+# #############################################################################
+# Initialize
+# #############################################################################
+SCRIPT_NAME="${0##*/}"
+SCRIPT_DIR="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# Load common functions
+source ${SCRIPT_DIR}/_functions.sh
+
+if [ -z "${JMETER_SCRIPT}" ]; then
+  echo JMETER_SCRIPT environment variable is not defined
+  exit 1
+fi
+
+JMETER_IMAGE=${JMETER_IMAGE:-exoplatform/jmeter}
+JMETER_IMAGE_VERSION=${JMETER_IMAGE_VERSION:-latest}
+
+JMETER_HEAP=${JMETER_HEAP:-512m}
+
+JMETER_SCRIPT_DIR=${JMETER_SCRIPT_DIR:-$(pwd)}
+JMETER_REPORT_DIR=${JMETER_REPORT_DIR:-$(pwd)}/results
+
+## Dynamically building the test environment
+PARAMETER_PREFIX="BENCHENV_"
+
+# WARNING Not supporting spaces
+PARAMETERS=$(env | grep ${PARAMETER_PREFIX})
+
+PARAMETER_STRING=""
+for i in ${PARAMETERS}
+do
+    PARAMETER_STRING="${PARAMETER_STRING} -J$(echo ${i} | cut -f2- -d"_")"
+done
+
+echo Creating report directory ${JMETER_REPORT_DIR}
+mkdir -p -v ${JMETER_REPORT_DIR}
+
+echo Running script with additional parameters : ${PARAMETER_STRING}
+
+echo ###################################################################################################################
+echo Beginning of the test : $(date)
+echo ###################################################################################################################
+
+DOCKER_ID=$(${DOCKER_CMD} run -d  \
+  -v ${JMETER_SCRIPT_DIR}:/jmeter \
+  -v ${JMETER_REPORT_DIR}:/output \
+  -e HEAP="-Xms${JMETER_HEAP} -Xmx${JMETER_HEAP}" \
+  ${JMETER_IMAGE}:${JMETER_IMAGE_VERSION} \
+  -n \
+  -t /jmeter/${JMETER_SCRIPT} \
+  -o /output/results \
+  -j /output/jmeter.log \
+  -l /output/sample.log \
+  ${PARAMETER_STRING})
+
+echo "Jmeter container id : ${DOCKER_ID}"
+
+trap "docker kill ${DOCKER_ID}; docker rm -v ${DOCKER_ID}" EXIT
+
+${DOCKER_CMD} logs -f ${DOCKER_ID}
+
+echo ###################################################################################################################
+echo End of the test : $(date)
+echo ###################################################################################################################
+
+# Clean the trap on normal exit
+trap - EXIT
