27fa670813d3419de9cebfa1d8c17fb41ebe0449
Vincent Sellier <vsellier@exoplatform.com>
2018-09-13 19:40:08 +0200

    Intermediate commit

---
+++ bin/init_stack.sh
@@ -0,0 +1,90 @@
+#!/usr/bin/env bash -eu
+
+# #############################################################################
+# Initialize
+# #############################################################################
+SCRIPT_NAME="${0##*/}"
+SCRIPT_DIR="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+BASE_DIR="$(dirname ${SCRIPT_DIR})"
+
+# Load common functions
+source ${SCRIPT_DIR}/_functions.sh
+source ${SCRIPT_DIR}/_plf_functions.sh
+
+echo "*** Environment : ${ENVIRONMENT} ***"
+
+[ "${DEBUG}" ] && set -x
+
+mkdir -p ${INSTANCE_DIR}
+pushd ${INSTANCE_DIR}
+
+echo Building docker-compose configuration
+
+#### Configuration ####
+SERVICES="front db mongo search mail plf1 jmxtrans1"
+##### PLF #####
+addOrReplaceEnvProperty EXO_NODE_COUNT 1
+addOrReplaceEnvProperty EXO_REGISTRATION false
+addOrReplaceEnvProperty EXO_DB_POOL_IDM_INIT_SIZE  1
+addOrReplaceEnvProperty EXO_DB_POOL_IDM_MAX_SIZE  10
+addOrReplaceEnvProperty EXO_DB_POOL_JCR_INIT_SIZE  2
+addOrReplaceEnvProperty EXO_DB_POOL_JCR_MAX_SIZE  5
+addOrReplaceEnvProperty EXO_DB_POOL_JPA_INIT_SIZE  3
+addOrReplaceEnvProperty EXO_DB_POOL_JPA_MAX_SIZE  20
+
+addOrReplaceEnvProperty EXO_CLUSTER_IP_RANGE "172.16.251.0/24"
+addOrReplaceEnvProperty EXO_CLUSTER_INSTANCE_IP_PREFIX 172.16.251
+
+forceEnvProperty NODES_NAMES "plf1"
+forceEnvProperty NODES_IPS   "${EXO_CLUSTER_INSTANCE_IP_PREFIX}.11"
+
+if [ ${EXO_NODE_COUNT} -gt 1 ]; then
+    for i in $(seq 2 $EXO_NODE_COUNT); do
+        forceEnvProperty NODES_NAMES "${NODES_NAMES},plf${i}"
+        forceEnvProperty NODES_IPS   "${NODES_IPS},${EXO_CLUSTER_INSTANCE_IP_PREFIX}.$((${i} +10))"
+
+        SLAVE_COMPOSE_FILES="${SLAVE_COMPOSE_FILES} -f ${INSTANCE_DIR}/compose-fragment/docker-compose-plf-node${i}.yml"
+        SLAVE_SERVICES="${SLAVE_SERVICES} plf${i} jmxtrans${i}"
+    done
+fi
+
+plfTemplate 1
+if [ ${EXO_NODE_COUNT} -gt 1 ]; then
+    for i in $(seq 2 $EXO_NODE_COUNT); do
+        plfTemplate $i
+    done
+fi
+
+# Apache Workers
+addOrReplaceEnvProperty APACHE_THREAD_PER_CHILD 20
+addOrReplaceEnvProperty APACHE_SERVER_LIMIT     25
+addOrReplaceEnvProperty APACHE_ASYNC_REQUEST_WORKER 2
+addOrReplaceEnvProperty APACHE_MAX_REQUEST_WORKER 500
+
+#### Apache compose ####
+template compose/docker-compose-apache.yml compose-fragment/docker-compose-apache.yml
+
+#### Apache configuration ####
+# Balancing between the number of nodes
+mkdir -p ${INSTANCE_DIR}/config/apache
+mkdir -p ${INSTANCE_DIR}/config/apache/include
+cp -rfv ${CONF_DIR}/apache ${INSTANCE_DIR}/config
+#cp -f ${CONF_DIR}/apache/include/proxy.conf ${INSTANCE_DIR}/config/apache/include
+template config/apache/httpd.conf
+template config/apache/include/proxy.conf config/apache/include/proxy.conf
+
+# TODO Parameterization
+# - database configuration
+# - PLF propertiesÎ
+# - optional config file
+# - patch
+# - PLF version
+${COMPOSE_CMD} \
+   -f ${TEMPLATE_DIR}/compose/docker-compose-mysql.yml \
+   -f ${INSTANCE_DIR}/compose-fragment/docker-compose-plf-node1.yml \
+   -f ${TEMPLATE_DIR}/compose/docker-compose-search.yml \
+   -f ${TEMPLATE_DIR}/compose/docker-compose-mongo.yml \
+   -f ${TEMPLATE_DIR}/compose/docker-compose-mail.yml \
+   -f ${INSTANCE_DIR}/compose-fragment/docker-compose-apache.yml \
+   ${SLAVE_COMPOSE_FILES} \
+   config > ${INSTANCE_DIR}/docker-compose.yml
